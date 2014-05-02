fc6bd3a1d5c84beb100a78320fb9505b70c65f41
신동환 <@SDH-PC>
2014-05-02 22:09:23 +0900

    GetTasksList 클래스를 이용할 수 있도록 변경

--- hwOAuth2/src/GoogleAPI/Tasks/TasksService.java
+++ hwOAuth2/src/GoogleAPI/Tasks/TasksService.java
@@ -1,5 +1,38 @@
 package GoogleAPI.Tasks;
 
-public class TasksService {
+import java.util.concurrent.ExecutionException;
 
+import com.google.api.services.tasks.model.TaskList;
+import com.google.api.services.tasks.model.TaskLists;
+
+import Oauth2.Credential;
+
+public class TasksService{
+
+	public TaskLists getTasksList(Credential credential) {
+		
+		TaskLists taskLists = null;
+		try {
+			taskLists = new GetTasksList().execute(credential).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return taskLists;
+	}
+	
+	public void showTasksList(TaskLists taskLists) {
+		
+		for (TaskList taskList : taskLists.getItems()) {
+			  System.out.println(taskList.getTitle());
+		}
+	}
+	
+	public void addTasksList(Credential credential, TaskList taskList) {
+		
+	}
 }

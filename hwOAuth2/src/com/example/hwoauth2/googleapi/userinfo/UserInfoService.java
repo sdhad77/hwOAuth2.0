642fe370c9a0d80e0fe6a0778795454616b88e6c
신동환 <@SDH-PC>
2014-05-05 07:21:13 +0900

    통합된 UserInfoService 클래스

---
+++ hwOAuth2/src/com/example/hwoauth2/googleapi/userinfo/UserInfoService.java
@@ -0,0 +1,75 @@
+package com.example.hwoauth2.googleapi.userinfo;
+
+import com.example.hwoauth2.R;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import Json.JsonParser;
+import Oauth2.Oauth2Info;
+import Oauth2.Oauth2Service;
+
+public class UserInfoService extends Activity
+{
+	private static UserInfoService _instance;
+	
+	public static UserInfoService getInstance()
+	{
+		if( _instance == null )
+		{
+			_instance = new UserInfoService();
+		}
+
+		return _instance;
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.userinfo_activity);
+		
+		Intent intent = getIntent();
+		
+		androidUniversalImageLoaderInit();
+		
+		Object[] jsonTempObject = new Object[3];
+	   	JsonParser jsonParser = new JsonParser();
+	    jsonTempObject = jsonParser.parse(intent.getStringExtra("jsonStr"));
+	  
+		ImageView imgView = (ImageView)findViewById(R.id.imageView);
+		
+		ImageLoader.getInstance().displayImage(jsonTempObject[2].toString(), imgView);
+			
+		TextView textView = (TextView) findViewById(R.id.textView1);
+		textView.setText(jsonTempObject[0].toString());
+		
+		textView = (TextView) findViewById(R.id.textView2);
+		textView.setText(jsonTempObject[1].toString());
+	}
+	
+	private void androidUniversalImageLoaderInit()
+	{
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+												.threadPriority(Thread.NORM_PRIORITY - 2)
+												.denyCacheImageMultipleSizesInMemory()
+												.discCacheFileNameGenerator(new Md5FileNameGenerator())
+												.tasksProcessingOrder(QueueProcessingType.LIFO)
+												.writeDebugLogs()
+												.build();
+	
+		ImageLoader.getInstance().init(config);
+	}
+	
+	public String getUserInfo()
+	{
+		return Oauth2Service.getInstance().accessProtectedResource(Oauth2Info.ENDPOINT_URL);
+	}
+}

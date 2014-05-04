39723bd82a3373d14b4e8fd0f98ab1a7d79c761c
신동환 <@SDH-PC>
2014-05-05 07:20:41 +0900

    fragment_main 부분 삭제 및 메인클래스 간소화 작업중.

--- hwOAuth2/src/com/example/hwoauth2/MainActivity.java
+++ hwOAuth2/src/com/example/hwoauth2/MainActivity.java
@@ -1,167 +1,80 @@
 package com.example.hwoauth2;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.services.tasks.model.TaskList;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.example.hwoauth2.googleapi.tasks.TasksService;
+import com.example.hwoauth2.googleapi.userinfo.UserInfoService;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
-import GoogleAPI.Tasks.TasksService;
-import Oauth2.Credential;
-import Oauth2.CredentialService;
 import Oauth2.Oauth2Info;
 import Oauth2.Oauth2Service;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-	
-	private Oauth2Service _oauth2;
-	private WebView _webView;
-	private Credential _credential;
-	private CredentialService _credentialService;
+public class MainActivity extends Activity
+{
+	private WebView _loginWebView;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-				.add(R.id.container, new PlaceholderFragment())
-				.commit();
-		}
         
 		init();
         
-		String oauth2Url = _oauth2.getAuthorizationUrl();
-		_webView.loadUrl(oauth2Url);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-        
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-			return rootView;
-		}
+		_loginWebView.loadUrl(Oauth2Service.getInstance().getAuthorizationUrl());
 	}
 	
-	private void init() {
+	private void init()
+	{
+		_loginWebView = (WebView) findViewById(R.id.webView1);
+		_loginWebView.getSettings().setJavaScriptEnabled(true);
 		
-		_oauth2 = new Oauth2Service();
-		_credential = new Credential();
-		_credentialService = new CredentialService();
-		
-		androidUniversalImageLoaderInit();
-		
-		_webView = (WebView) findViewById(R.id.webView1);
-		_webView.getSettings().setJavaScriptEnabled(true);
-		
-		_webView.setWebViewClient(new WebViewClient() {			
-			public void onPageFinished(WebView view, String url) {
+		_loginWebView.setWebViewClient(new WebViewClient()
+		{			
+			public void onPageFinished(WebView view, String url)
+			{
 				super.onPageFinished(view, url);
-				if(url.startsWith(Oauth2Info.REDIRECT_URI)) {
-					if (url.indexOf("code=") != -1) {
-							
+				if(url.startsWith(Oauth2Info.REDIRECT_URI))
+				{
+					if (url.indexOf("code=") != -1)
+					{
 						String code = url.substring(Oauth2Info.REDIRECT_URI.length() + 7, url.length());
 							
-						GoogleTokenResponse accessToken = _oauth2.accessTokenRequest(code);
-						_credential.setCredential(_credentialService.makeCredential(accessToken));
-						String result = _oauth2.accessProtectedResource(_credential);
-							
-						Object[] jsonTempObject = new Object[3];
-				       	JsonParser jsonParser = new JsonParser();
-				       	jsonTempObject = jsonParser.parse(result);
+						Oauth2Service.getInstance().codeToCredential(code);
 	
-			//	       	showUserInfo(jsonTempObject);
+						_loginWebView.setVisibility(View.INVISIBLE);
+						
+				       	showUserInfo();
 				       	
-				       	showTask();
+		//		       	showTask();
 					}
-					else if (url.indexOf("error=") != -1) {
+					else if (url.indexOf("error=") != -1)
+					{
 						Toast.makeText(getApplicationContext(), "code error", Toast.LENGTH_LONG).show();
-						_webView.setVisibility(View.INVISIBLE);
+						_loginWebView.setVisibility(View.INVISIBLE);
 					}
 				}
 			}
 		});
 	}
-	
-	private void androidUniversalImageLoaderInit() {
-		   
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
-												.threadPriority(Thread.NORM_PRIORITY - 2)
-												.denyCacheImageMultipleSizesInMemory()
-												.discCacheFileNameGenerator(new Md5FileNameGenerator())
-												.tasksProcessingOrder(QueueProcessingType.LIFO)
-												.writeDebugLogs()
-												.build();
-
-		ImageLoader.getInstance().init(config);
-	}
    
-	private void showUserInfo(Object[] obj) {
-	   
-		WebView webView = (WebView) findViewById(R.id.webView1);
-		webView.setVisibility(View.INVISIBLE);
-		
-		ImageView imgView = (ImageView)findViewById(R.id.imageView1);
-		
-		imgView.setVisibility(View.VISIBLE);
-		ImageLoader.getInstance().displayImage(obj[2].toString(), imgView);
-			
-		TextView textView = (TextView) findViewById(R.id.textView1);
-		textView.setVisibility(View.VISIBLE);
-		textView.setText(obj[0].toString() + "\n" + obj[1].toString());
+	private void showUserInfo()
+	{
+		Intent intent = new Intent(getApplicationContext(), UserInfoService.class);
+		intent.putExtra("jsonStr", UserInfoService.getInstance().getUserInfo());
+		startActivity(intent);
+		finish();
 	}
 	
-	private void showTask() {
-		
-		TaskList tempTask = new TaskList();
-       	tempTask.setTitle("ADD Test");
-       	TasksService service = new TasksService();
-       	service.showTasksList(service.getTasksList(_credential));
-       	service.addTasksList(_credential, tempTask);
-       	service.showTasksList(service.getTasksList(_credential));
+	private void showTask()
+	{
+       	Intent intent = new Intent(getApplicationContext(), TasksService.class);
+		startActivity(intent);
+		finish();
 	}
 }

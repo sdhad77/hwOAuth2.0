b6e01f17d8ef6397cba43070a188526178a15494
신동환 <@SDH-PC>
2014-05-06 07:05:23 +0900

    public static으로 설정해놓았던 API_Key등 문자열들을 private으로 변경해줌.

--- hwOAuth2/src/Oauth2/Oauth2Info.java
+++ hwOAuth2/src/Oauth2/Oauth2Info.java
@@ -5,16 +5,50 @@ import java.util.List;
 
 public class Oauth2Info
 {
-	final public static String CLIENT_ID = "438691373590-52ct0cdjpic99f80orje850gutg1elg6.apps.googleusercontent.com";
-	final public static String CLIENT_SECRET = "";
-	final public static String API_KEY = "AIzaSyCz-ZbhCQwM5mEJEGWkPI8HhoUwklHoGIc";
+	final private String CLIENT_ID = "438691373590-52ct0cdjpic99f80orje850gutg1elg6.apps.googleusercontent.com";
+	final private String CLIENT_SECRET = "";
+	final private String API_KEY = "AIzaSyCz-ZbhCQwM5mEJEGWkPI8HhoUwklHoGIc";
+	final private String REDIRECT_URI = "http://localhost";
 	
-	final public static String ENDPOINT_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
-	final public static String REDIRECT_URI = "http://localhost";
+	private String ENDPOINT_URL;
+	private List<String> SCOPE;
 	
-	final public static List<String> SCOPE = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
-														   "https://www.googleapis.com/auth/userinfo.email",
-														   "https://www.googleapis.com/auth/tasks"
-															);
+	private String ENDPOINT_UserInfo = "https://www.googleapis.com/oauth2/v3/userinfo";
+	private String ENDPOINT_Tasks    = "https://www.googleapis.com/oauth2/v3/userinfo";
+	
+	private List<String> SCOPE_UserInfo = Arrays.asList("https://www.googleapis.com/auth/userinfo.profile",
+														"https://www.googleapis.com/auth/userinfo.email");
+	private List<String> SCOPE_Tasks    = Arrays.asList("https://www.googleapis.com/auth/tasks");
+	
+	private static Oauth2Info _instance;
 
+	private Oauth2Info() {};
+	
+	public static Oauth2Info getInstance()
+	{
+		if( _instance == null )
+		{
+			_instance = new Oauth2Info();
+		}
+
+		return _instance;
+	}
+	
+	public String getCLIENT_ID()            {return CLIENT_ID;}
+	public String getCLIENT_SECRET()        {return CLIENT_SECRET;}
+	public String getAPI_KEY()              {return API_KEY;}
+	public String getREDIRECT_URI()         {return REDIRECT_URI;}
+	public String getENDPOINT_URL()         {return ENDPOINT_URL;}
+	public List<String> getSCOPE()          {return SCOPE;}
+	public String getENDPOINT_UserInfo()    {return ENDPOINT_UserInfo;}
+	public String getENDPOINT_Tasks()       {return ENDPOINT_Tasks;}
+	public List<String> getSCOPE_UserInfo() {return SCOPE_UserInfo;}
+	public List<String> getSCOPE_Tasks()    {return SCOPE_Tasks;}
+	
+	public void setENDPOINT_URL(String eNDPOINT_URL)           {ENDPOINT_URL = eNDPOINT_URL;}
+	public void setSCOPE(List<String> sCOPE)                   {SCOPE = sCOPE;}
+	public void setENDPOINT_UserInfo(String eNDPOINT_UserInfo) {ENDPOINT_UserInfo = eNDPOINT_UserInfo;}
+	public void setENDPOINT_Tasks(String eNDPOINT_Tasks)       {ENDPOINT_Tasks = eNDPOINT_Tasks;}
+	public void setSCOPE_UserInfo(List<String> sCOPE_UserInfo) {SCOPE_UserInfo = sCOPE_UserInfo;}
+	public void setSCOPE_Tasks(List<String> sCOPE_Tasks)       {SCOPE_Tasks = sCOPE_Tasks;}
 }
